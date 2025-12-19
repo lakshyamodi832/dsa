b279f510113ed4d7c44f8a580bec10e76b9df6f8
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-19 13:24:13 +0530

    revArr

---
+++ arr/revArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void revArray(int arr[], int size){
+    int start = 0;
+    int end = size - 1;
+
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    
+    int rev[] = {1, 2, 3, 4, 5};
+    int size = 5;
+
+    revArray(rev, size);
+
+    for(int i = 0; i < size; i++){
+        cout<<rev[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
