0343a17a7315a66140b98c26143bb9e836d3d4f8
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-21 19:21:10 +0530

    pushback

--- vector/functions.cpp
+++ vector/functions.cpp
@@ -15,8 +15,14 @@ int main(){
 
     cout<<"size = "<<vec.size()<<endl;//0    
 
-    for(char val : vec){
-        cout<<val<<endl;
+    vec.push_back(25);
+    vec.push_back(35);
+    vec.push_back(45);
+
+    cout<<"size after push back = "<<vec.size()<<endl;//3
+
+    for(int val : vec){
+        cout<<val<<endl;//25, 35, 45
     }
 
     return 0;
