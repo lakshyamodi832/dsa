3117ec7ed5a0459d55e3744467f647df8d4502e9
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-21 15:57:41 +0530

    vecDeclare

--- vector/syntax.cpp
+++ vector/syntax.cpp
@@ -7,5 +7,8 @@ int main(){
     vector<int>vec;//sz=0
     cout<<vec[0]<<endl;//segmentation fault(cannot access what has not been created yet)
 
+    vector<int>vect = {1, 2, 3};
+    cout<<vect[0]<<endl;//1
+
     return 0;
 }
