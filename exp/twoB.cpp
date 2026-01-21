daee081f47252abb3ed332710740918cde2b6851
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-21 17:54:06 +0530

    twoB

--- exp/twoB.cpp
+++ exp/twoB.cpp
@@ -18,7 +18,7 @@ int main(){
     int arr[7] = {2, 3, 1, 4, 5, 7, 6};
     int n = 7;
 
-    selection(arr, n);
+    selection (arr, n);
 
     for(int i = 0; i < n; i++){
         cout<<arr[i]<<endl;
