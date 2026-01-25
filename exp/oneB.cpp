dcb37c8eac096cd7ae157772bf98d467c4738549
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-25 22:08:40 +0530

    oneB

--- exp/oneB.cpp
+++ exp/oneB.cpp
@@ -6,7 +6,7 @@ int binary(vector<int> arr, int tar){
     int st = 0, end = arr.size()-1;
 
     while(st <= end){
-        int mid = (st + end)/2;
+        int mid = st + ((end - st)/2);
         if(tar > arr[mid]){
             st = mid + 1;
         } else if(tar < arr[mid]){
