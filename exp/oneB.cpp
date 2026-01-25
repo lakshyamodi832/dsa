8e849f6888be0fbf2297ca51c692056458766679
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-25 22:42:03 +0530

    oneB

--- exp/oneB.cpp
+++ exp/oneB.cpp
@@ -2,7 +2,21 @@
 #include<vector>
 using namespace std;
 
-int binary(vector<int> arr, int tar){
+int recBinary(vector<int> arr, int tar, int st, int end){
+    if(st<=end){
+        int mid = st + ((end - st)/2);
+        if(tar < arr[mid]){
+            return recBinary(arr, tar, st, mid - 1);
+        } else if(tar > arr[mid]){
+            return recBinary(arr, tar, mid + 1, end);
+        }else{
+            return mid; 
+        }
+    }
+    return -1;
+}
+
+int iterBinary(vector<int> arr, int tar){
     int st = 0, end = arr.size()-1;
 
     while(st <= end){
@@ -24,9 +38,13 @@ int main(){
 
     vector<int> v2 = {-1, 0, 3, 5, 9, 12};
 
-    cout<<binary(v1, 3)<<endl;
+    cout<<iterBinary(v1, 3)<<endl;
 
-    cout<<binary(v2, 12)<<endl;
+    cout<<iterBinary(v2, 12)<<endl;
+
+    cout<<recBinary(v1, 3, 0, v1.size() - 1)<<endl;
+
+    cout<<recBinary(v2, 12, 0, v2.size() - 1)<<endl;
 
     return 0;
 }
